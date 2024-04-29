30d622fa465856544ebb46f3df70f53374c21bd0
erick11 <erick.eduardo.ee7@gmail.com>
2024-04-28 20:01:21 -0500

    chore: second commit basic folder structure

---
+++ src/test/java/com/bemainick/reservations/ApiReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemainick.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
